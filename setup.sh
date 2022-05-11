0c7ad812d7df3a764302b5565fff238d37365e49
testuser-at-490752553772 <m_aronis@hotmail.com>
2022-05-11 17:51:14 +0300

    first

---
+++ setup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -e
+set -x
+
+wdir=$(pwd)
+user=$(id -un 1000)
+#sudo chown -R $user:$user *
+
+git clone https://github.com/appliedzkp/zkevm-chain.git
+
+docker run -v $wdir/zkevm-chain/contracts:/sources ethereum/solc:stable --abi /sources/ZkEvmL1Bridge.sol -o /sources/build
+docker run -dit -v $wdir:/Code --name gotest golang
+docker exec --workdir /Code/TestCode gotest /Code/go_init.sh  
+cd $wdir/zkevm-chain/contracts/build/
+
+for i in `ls -p`
+    do
+        outfile=$(echo $i | sed 's/.abi/.go/g')
+        n=$(echo ${i} | sed 's/\.[^ ]*/ /g')
+        nf=$(echo "/Code/TestCode/${n}")
+        mkdir -p $nf
+        docker exec --workdir /Code/zkevm-chain/contracts/build/ gotest abigen --abi $i -pkg $n --type $n --out $outfile
+        docker exec --workdir /Code gotest mkdir -p $nf 
+        docker exec --workdir /Code/zkevm-chain/contracts/build/ gotest cp $outfile $nf
+    done
+
+for i in `find $wdir/TestCode -type d -exec basename {} \;`
+    do
+        pack=${i,,}
+        echo "replace $pack v1.0.0 => ./$i" >> $wdir/TestCode/go.mod
+#       echo "replace $pack v1.0.0 => ./$i" >> $wdir/go.mod
+    done
+
+docker exec --workdir /Code/TestCode gotest go mod tidy
+
+sudo chown -R $user:$user *
