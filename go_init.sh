3d1f2d9d05a0507c6e833f4d244820e234f9d474
AronisAt79 <45406504+AronisAt79@users.noreply.github.com>
2022-05-26 13:07:03 +0300

    Update go_init.sh

--- go_init.sh
+++ go_init.sh
@@ -4,5 +4,5 @@ set -x
 
 go mod init zkevmchaintest
 go get -u github.com/ethereum/go-ethereum
-cd /go/pkg/mod/github.com/ethereum/go-ethereum\@v1.10.17/
+cd /go/pkg/mod/github.com/ethereum/go-ethereum\@v1.10.18/
 make devtools 
